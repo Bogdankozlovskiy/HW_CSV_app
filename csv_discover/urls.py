4cf39cec607df96623e815b8651f3a63d15306f3
Bogdan <bkozlovsky@bk.ru>
2020-10-21 14:28:59 +0300

    left last part

--- csv_discover/urls.py
+++ csv_discover/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from csv_discover.views import UploadFile
+from csv_discover.views import UploadFile, AggregateTable
 
 urlpatterns = [
+    path("get_aggregate", AggregateTable.as_view(), name='aggregate_table'),
     path("", UploadFile.as_view(), name='upload_file'),
 ]
--- csv_discover/views.py
+++ csv_discover/views.py
@@ -13,12 +13,25 @@ class UploadFile(View):
 
     def post(self, request):
         form = FileForm(request.POST, request.FILES)
+        response = {}
         if form.is_valid():
             file_csv = form.save()
-            df = pd.read_csv(file_csv.file).to_html()
-            df = mark_safe(df)
-            file_name = file_csv.file.__str__().split('/')[-1]
-            form = FileForm()
-            return render(request, 'index.html', {'df': df, 'file_name': file_name, 'form': form})
+            df = pd.read_csv(file_csv.file, sep=' ', index_col=0).to_html()
+            response['df'] = mark_safe(df)
+            response['file_name'] = file_csv.file.__str__().split('/')[-1]
+            response['form'] = FileForm()
+            response['row_field'] = ['Date']
+            response['column_field'] = ['Product', 'Sales', 'Person']
+            response['value_field'] = ['Qty', 'Total']
+            return render(request, 'index.html', response)
         return redirect('upload_file')
+
+
+class AggregateTable(View):
+    def get(self, request):
+        row_field = request.GET['row_field']
+        column_field = request.GET['column_field']
+        value_field = request.GET['value_field']
+        print(row_field, column_field, value_field)
+        return render(request, 'aggregate_table.html')
 # Create your views here.
