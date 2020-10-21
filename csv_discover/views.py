8e31ee31871c54121aa01ec4b4a8179bf1f3e12a
Bogdan <bkozlovsky@bk.ru>
2020-10-21 17:00:23 +0300

    little fix

--- csv_discover/views.py
+++ csv_discover/views.py
@@ -16,7 +16,7 @@ class UploadFile(View):
         response = {}
         if form.is_valid():
             file_csv = form.save()
-            df = pd.read_csv(file_csv.file, sep=' ', index_col=0)
+            df = pd.read_csv(file_csv.file, sep=';', index_col=0)
             response['df'] = mark_safe(df.to_html())
             response['file_name'] = file_csv.file.__str__().split('/')[-1]
             response['full_name'] = file_csv.file.__str__()
@@ -34,7 +34,7 @@ class AggregateTable(View):
         column_field = request.GET['column_field']
         value_field = request.GET['value_field']
         full_name = f"./media/{request.GET['full_name']}"
-        df = pd.read_csv(full_name, sep=' ', index_col=0)
+        df = pd.read_csv(full_name, sep=';', index_col=0)
         result = df.groupby([row_field, column_field]).agg({value_field: 'sum'}).to_html()
         result = mark_safe(result)
         return render(request, 'aggregate_table.html', {'result': result})
